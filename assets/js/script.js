b6ed694e82c10c063cbdd12ef6d7008330feb631
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2022-11-25 18:05:12 -0800

    HTML, CSS, JS files added to repo

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var APIKey = '3a6c0dad8c31e32a34e1dd280e179896';
+
+// fetch("https://api.github.com/repos/nodejs/node/issues?per_page=5", {
+//   cache: "reload",
+// })
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
