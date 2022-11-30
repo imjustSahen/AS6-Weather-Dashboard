0c6cb81f9ca2fa92db14d5bce0a3c80b80f72bc5
imjustSahen <114109203+imjustSahen@users.noreply.github.com>
2022-11-29 21:38:39 -0800

    updated HTML and CSS files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 var APIKey = '3a6c0dad8c31e32a34e1dd280e179896';
+var weatherApiRootUrl = 'https://api.openweathermap.org';
+var searchHistory = [];
+var city;
 
-// fetch("https://api.github.com/repos/nodejs/node/issues?per_page=5", {
-//   cache: "reload",
-// })
+// fetch("api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey)
 //   .then(function (response) {
-//     return response.json();
+//     return response.JSON();
 //   })
 //   .then(function (data) {
 //     console.log(data);
